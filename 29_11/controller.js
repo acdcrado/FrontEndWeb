40dc96385d4d10b39dee4ee34d97ea5c643af9f4
acdcrado <62611119+acdcrado@users.noreply.github.com>
2020-11-29 12:47:31 +0200

    Add files via upload
    added update

--- 29_11/controller.js
+++ 29_11/controller.js
@@ -1,101 +1,4 @@
 
-	const initDatabase = () => {
-		const config = {
-			apiKey: "AIzaSyDamu-YcK408nYTP1MG4hNdrXpyU-R4KL8",
-			authDomain: "flutter-38595.firebaseapp.com",
-			databaseURL: "https://flutter-38595.firebaseio.com",
-			projectId: "flutter-38595",
-			storageBucket: "",
-			messagingSenderId: "114656105297"
-		};
-		firebase.initializeApp(config);
-	};
-	initDatabase();
-
-	/**
-	 * Log-ins with provided information.
-	 * Parameters:
-	 * - email <string> - email to be used
-	 * - password <string> - password to be user
-	 * - callback <function> - function to call once the login operation is completed.
-	 *	The parameters for this function are:
-	 *		- successfull <boolen> true if log-in is successfull, false otherwise
-	 *  	- errorCode <number> the error code in case sucessfull = false
-	 *		- errorMessage <string> the error message in case sucessfull = false
-	 **/
-	const login = (email, password, callback) => {
-		return firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
-			// Success - redirect
-			callback(true);
-		}, (error) => {
-			const errorCode = error.code;
-			//Here we use the error message like it is returned by the firebase (the message is in english)
-			const errorMessage = error.message;
-
-			// handle error with login
-			callback(false, errorCode, errorMessage);
-		});
-	};
-
-	const logout = () => {
-		return firebase.auth().signOut();
-	};
-
-	/**
-	 * @callback registerCallback
-	 * @param {boolean} successful if log-in is successful, false otherwise
-	 * @param {number} errorCode the error code in case successful = false
-	 * @param {string} errorMessage the error message in case successful = false
-	 */
-
-	/**
-	 * Register given user with given information
-	 *
-	 * @param {string} username
-	 * @param {string} email
-	 * @param {string} password
-	 * @param {registerCallback} callback function to call once the register operation is completed
-	 *
-	 **/
-	const register = (username, email, password, callback) => {
-		firebase.auth().createUserWithEmailAndPassword(email, password).then((data) => {
-			data.user.updateProfile({
-				displayName: username
-			}).then(function () {
-				callback(true);
-			}, function (error) {
-				console.log(error);
-			});
-		}, (error) => {
-			const errorCode = error.code;
-			let errorMessage;
-
-			// Here we analyze the code and set custom error message (in bulgarian) just for the sake of the exersice
-			switch (errorCode) {
-				case 'auth/weak-password':
-					{
-						errorMessage = "Registration failed. Weak password.";
-						break;
-					}
-				case 'auth/email-already-in-use':
-					{
-						errorMessage = "Registration failed. Email is already used.";
-						break;
-					}
-				case 'auth/invalid-email':
-					{						
-						errorMessage = "Registration failed. Email is not valid.";
-						break;
-					}
-				default:
-					{
-						errorMessage = "Registration failed.";
-					}
-			}
-			callback(false, errorCode, errorMessage);
-		});
-	};
-
 function verify(){
 
 function verify_email(){
@@ -150,7 +53,7 @@ function verify_pass()
             return false;
         }
         //console.log(document.getElementById("un1").value, document.getElementById("email").value, document.getElementById("pass1").value)
-        register(document.getElementById("un1").value, document.getElementById("email").value, document.getElementById("pass1").value);
+        auth.register(document.getElementById("un1").value, document.getElementById("email").value, document.getElementById("pass1").value);
         
         
         alert("You have successfully registered.");
